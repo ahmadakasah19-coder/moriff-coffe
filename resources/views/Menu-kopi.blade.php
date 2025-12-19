7d2fc5643c2b4ae4586b57750a41354d00a9e60f
ahmadakasah19-coder <ahmadakasah19@gmail.com>
2025-12-19 11:21:18 +0800

    barista

--- resources/views/Menu-kopi.blade.php
+++ resources/views/Menu-kopi.blade.php
@@ -9,7 +9,11 @@
         <p>{{ $m->deskripsi }}</p>
         <strong>Harga: Rp {{ number_format($m->harga) }}</strong>
         <p>Jenis Kopi: {{ $m->jenis->nama_jenis }}</p>
-        <p>Barista: {{ $m->barista->nama_barista }}</p>
+@if($m->barista)
+    <p>Barista: {{ $m->barista->nama_barista }}</p>
+@else
+    <p>Barista: -</p>
+@endif
         @if($m->gambar)
             <br><img src="{{ asset('storage/'.$m->gambar) }}" width="150">
         @endif
